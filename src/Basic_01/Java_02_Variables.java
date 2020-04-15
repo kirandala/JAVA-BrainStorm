24491edd7d3638481f1628d7c61517830c316e51
kirandalai <kirandalai@localhost>
2020-04-16 01:19:39 +0530

    Data types added with examples

---
+++ src/Basic_01/Java_02_Variables.java
@@ -0,0 +1,16 @@
+package Basic_01;
+
+public class Java_02_Variables {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Hello , one");
+		// when we give something in "" - its string literal and we are trying to tell java that it would be a string literal
+		int number=5+6;
+		System.out.println("number"); // number_ Coz here number is string literal
+		System.out.println(number);  // 5 the variable will be called here 
+		int number1 =5;
+		System.out.println("After addition value of num is "+number+number1); // 16 it adds the new value aswell
+	
+	}
+}
