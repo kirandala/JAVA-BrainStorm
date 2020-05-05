f5f9dda19b502ed781256065cee5c0c21f28e2c3
kirandalai <kirandalai@localhost>
2020-05-05 14:00:45 +0530

    Reference Class object and Instance explained

---
+++ src/java_07_OOPs/ReferenceNobject/Java_12_RefereceObject.java
@@ -0,0 +1,24 @@
+package java_07_OOPs.ReferenceNobject;
+
+public class Java_12_RefereceObject {
+	/*
+	 * Analogy of building a house to understand class A class is basically a
+	 * blueprint for a house, using the blueprint(Plans) we can build as many houses
+	 * as we like based on the plans.
+	 * 
+	 * Each house you build ( in other words instantiate using the new operator) is
+	 * an object also know as a instance.
+	 * 
+	 * Each house you build has an address(a physical location). In other words if
+	 * you want to tell someone where you live, you give them your address (perhaps
+	 * written one piece of paper). this is known as reference.
+	 * 
+	 * you can copy that reference as many times as you like but there is still
+	 * just one house.In other words we are copying the paper that has address on it
+	 * not the house itself
+	 * 
+	 * 
+	 * 
+	 */
+
+}
