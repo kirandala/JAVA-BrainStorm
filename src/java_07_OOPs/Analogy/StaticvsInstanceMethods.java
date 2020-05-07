5a83c7b3bc31eb21a0e3727f166f6ebfe9211aeb
kirandalai <kirandalai@localhost>
2020-05-07 23:56:26 +0530

    Static and instance methods

---
+++ src/java_07_OOPs/Analogy/StaticvsInstanceMethods.java
@@ -0,0 +1,31 @@
+package java_07_OOPs.Analogy;
+
+/**
+ * @author kirandalai
+ *
+ */
+public class StaticvsInstanceMethods {
+
+	/**
+	 *Static methods are declared using a static modifier 
+	 *
+	 *Static methods Cant's access instance methods and instance variables directly 
+	 *
+	 *They are usualy user for operation that dont require any data from an instance of the class(from this)
+	 *
+	 *the this keyword is the current instance of the class so, we cant use this keyword.
+	 *
+	 *Whenever you see a method that does not use instance variables that method should be declared as static method.
+	 *
+	 *for example main is a static method and it is called by jvm when it starts an application .
+	 *
+	 *Static methods are classed as ClassName.methodname() or methodname() is in the same class.
+	 *
+	 *
+	 */
+	public static void main(String[] args) {
+
+
+	}
+
+}
